d0d5d061129c9be542fab2c5ee594549d737f377
Ben Slater <ben@overmation.com>
2015-12-11 14:22:26 +0000

    added helpers to format index view

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,11 @@
 module MoviesHelper
+
+	def format_total_gross(movie)
+		if movie.flop?
+			"<strong>Flop!</strong>".html_safe
+		else
+			number_to_currency(movie.total_gross)
+		end
+	end
+
 end
---
+++ config/initializers/date_formats.rb
@@ -0,0 +1,2 @@
+
+Date::DATE_FORMATS[:release_date] = "%B %e, %Y"
